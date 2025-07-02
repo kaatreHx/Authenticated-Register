f3d0ea723ae4f177957f8649c2e5a8a952851248
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-02 20:42:07 +0545

    Register user viewsets

--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,23 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
     class Meta:
         model = CustomUser
-        fields = ['email', 'phone', 'password']
+        fields = ['email', 'phone', 'password', 'confirm_password']
+    
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+    
+    def create(self, validated_data):
+        # Remove confirm_password before creating the user
+        validated_data.pop('confirm_password')
         
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            phone=validated_data['phone'],
+            password=validated_data['password']
+        )
+        return user
